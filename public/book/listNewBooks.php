c2d17674b9d2c1497d9fb14218deeb87aa066cd8
alban <alban@qodlab.io>
2022-12-07 16:32:42 +0100

    list new book with redis

--- public/book/listNewBooks.php
+++ public/book/listNewBooks.php
@@ -4,22 +4,25 @@
 //get vue via API
 //get vue via REDIS
 $books = newBooks();
-foreach ($books as $book) : ?>
+foreach ($books as $book) :
+    $isbn13 = $book->isbn13;
+    $bookRedis = getRedisBookByIsbn($isbn13);
+    ?>
     <div class="col">
         <div class="card shadow-sm">
-            <img src="<?= $book->image ?>" alt="<?= $book->image ?>">
+            <img src="<?= $bookRedis->image ?>" alt="<?= $bookRedis->image ?>">
             <div class="card-body">
-                <p class="card-title"><h4><?= $book->title ?></h4></p>
+                <p class="card-title"><h4><?= $bookRedis->title ?></h4></p>
                 <p class="card-text">
-                    <?= $book->subtitle ?>
+                    <?= $bookRedis->subtitle ?>
                     </br>Prix:
-                    <?= $book->price ?>
+                    <?= $bookRedis->price ?>
                     </br>ISBN :
-                    <?= $book->isbn13 ?>
+                    <?= $bookRedis->isbn13 ?>
                 </p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                        <a href="book/details.php?isbn=<?= $book->isbn13 ?>" type="button" class="btn btn-sm btn-outline-secondary">View</a>
+                        <a href="book/details.php?isbn=<?= $bookRedis->isbn13 ?>" type="button" class="btn btn-sm btn-outline-secondary">View</a>
                         <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                     </div>
                     <small class="text-muted">9 mins</small>
