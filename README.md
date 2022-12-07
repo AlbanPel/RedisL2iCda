03cd6390c163dedbc06da4f46513194b3b523557
alban <alban@qodlab.io>
2022-12-07 14:07:39 +0100

    redis full

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RedisL2iCda
 PHP 8 / Bootstrap 5.2/ Redis
+- webserver: localhost:8000
+- Redis port: 6379
--- public/redis.php
+++ public/redis.php
@@ -1,3 +1,35 @@
 <?php
     $redis = new \Redis();
     $redis->connect('redis',6379);
+    //$redis->set('formation', 'HELLO AIX NICE');
+    //echo $redis->get('formation');
+
+
+    include('api.php');
+    $books = newBooks();
+    foreach ($books as $book) {
+        //echo $book->isbn13;
+        $isbn13 = $book->isbn13;
+        $url = 'https://api.itbook.store/1.0/books/'.$isbn13;
+        $details = json_decode(file_get_contents($url));
+        $redis->hset('keyBook:'.$isbn13, 'title', $details->title);
+        $redis->hset('keyBook:'.$isbn13, 'subtitle', $details->subtitle);
+        $redis->hset('keyBook:'.$isbn13, 'price', $details->price);
+        $redis->hset('keyBook:'.$isbn13, 'image', $details->image);
+        $redis->hset('keyBook:'.$isbn13, 'isbn13', $isbn13);
+        $redis->hset('keyBook:'.$isbn13, 'description', $details->description);
+        $redis->hset('keyBook:'.$isbn13, 'authors', $details->authors);
+        $redis->hset('keyBook:'.$isbn13, 'publisher', $details->publisher);
+        $redis->hset('keyBook:'.$isbn13, 'pages', $details->pages);
+        $redis->hset('keyBook:'.$isbn13, 'year', $details->year);
+        $redis->hset('keyBook:'.$isbn13, 'rating', $details->rating);
+        $redis->hset('keyBook:'.$isbn13, 'reviews', $details->reviews);
+        $redis->hset('keyBook:'.$isbn13, 'language', $details->language);
+        $redis->hset('keyBook:'.$isbn13, 'url', $details->url);
+        $redis->hset('keyBook:'.$isbn13, 'pdf', $details->pdf);
+        $redis->hset('keyBook:'.$isbn13, 'epub', $details->epub);
+        $redis->hset('keyBook:'.$isbn13, 'mobi', $details->mobi);
+
+
+
+    }
